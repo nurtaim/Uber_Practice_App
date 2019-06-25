b6283c30228e8330343ccbe6e8e7226374c8273a
nurtaim <munzhurov@gmail.com>
2019-06-25 02:58:49 -0300

    first commit

---
+++ uber/app/src/main/java/com/Nurtai/BookMyRide/PayPalConfig.java
@@ -0,0 +1,7 @@
+package com.Nurtai.BookMyRide;
+
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "ASK78jaMX4qnie4agxoeNchDhZ6oAc-TB0mPo0ldt3dfJm8MUO5OYOYZ_axXRKJwMkdKJtV8zhlqHtUG";
+    public static final String PAYPAL_PAYOUT_URL = "https://us-central1-bookmyride-7c20b.cloudfunctions.net/payout";
+}
